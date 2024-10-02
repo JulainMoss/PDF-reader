7ea7f9b1214d0bb1895cea5da55c428b62237b58
Julian <julian.h.mossakowski@gmail.com>
2024-10-02 18:22:47 +0200

    Simple read with example pdf

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-if __name__ == "__main__":
-    print("hello world!!")
+import PyPDF2
+
+# Otwieranie pliku PDF
+with open('ExamplePDF.pdf', 'rb') as pdf_file:
+    reader = PyPDF2.PdfReader(pdf_file)
+    
+    # Odczytywanie liczby stron
+    num_pages = len(reader.pages)
+    
+    # Odczyt wszystkich stron PDF
+    for page_num in range(num_pages):
+        page = reader.pages[page_num]
+        text = page.extract_text()
+        print(f"Strona {page_num + 1}:")
+        print(text)
